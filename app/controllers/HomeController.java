05e1fa1d5f5a6330df12583d40f82ddd8d1e8823
KonnorSidler <36972046+KonnorSidler@users.noreply.github.com>
2019-03-18 20:21:33 -0400

    Init

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import play.mvc.*;
+import views.html.*;
+import models.Person;
+import play.data.FormFactory;
+import play.data.Form;
+import javax.inject.Inject;
+import java.util.List;
+import io.ebean.Model;
+import play.libs.Json;
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+  @Inject
+  FormFactory formFactory;
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+      Form<Person> personForm = formFactory.form(Person.class);
+
+      return ok(index.render(personForm));
+    }
+
+    public Result addPerson(Http.Request request) {
+      Form<Person> personForm = formFactory.form(Person.class).bindFromRequest(request);
+      Person person = personForm.get();
+      person.save();
+      return redirect(routes.HomeController.index());
+    }
+
+    public Result getPersons() {
+      List<Person> persons = Person.find.all();
+      return ok(Json.toJson(persons));
+    }
+
+}
---
+++ app/models/Person.java
@@ -0,0 +1,17 @@
+package models;
+
+import io.ebean.Model;
+import io.ebean.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person extends Model {
+
+  @Id
+  public long id;
+
+  public String name;
+
+  public static final Finder<Long, Person> find = new Finder<>(Person.class);
+}
