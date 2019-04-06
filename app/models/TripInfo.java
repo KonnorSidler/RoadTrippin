4cd8029a4931a4e737f513fc2d8cb8745635919d
WoodruffA122 <woodruff.r.alexander@gmail.com>
2019-04-06 16:47:14 -0400

    Added constraints for StopCount

--- app/models/TripInfo.java
+++ app/models/TripInfo.java
@@ -15,9 +15,17 @@ public class TripInfo extends Model {
 
     private String endLocation;
 
+    private final int minStopCount = 1;
+    private final int maxStopCount = 5;
     private int stopCount;
 
     public static final Finder<Long, TripInfo> find = new Finder<>(TripInfo.class);
 
+    public void setStopCount(int count) {
+        if((count < minStopCount) || (count > maxStopCount)) {
+            throw new IllegalArgumentException("rating value out of range");
+        }
+        this.stopCount = count;
+    }
     //calculation for average trip rating. potentially script
 }
