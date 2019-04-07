437580e5a0e511010b46f7e38ff1d1328a01084e
Thomas Shoff <thomasshoff14@gmail.com>
2019-04-07 13:56:52 -0400

    Fixed 403, moved ratings div code

--- public/javascripts/showRatingDiv.js
+++ public/javascripts/showRatingDiv.js
@@ -2,7 +2,5 @@ function showRatingDiv() {
     var x = document.getElementById("rating_div");
     if (x.style.display === "none") {
         x.style.display = "block";
-    } else {
-        x.style.display = "none";
     }
 }
