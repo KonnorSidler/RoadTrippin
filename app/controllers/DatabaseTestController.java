5901762952c3d211361799100aef3733aff7fab9
KonnorSidler <36972046+KonnorSidler@users.noreply.github.com>
2019-04-06 22:56:09 -0400

    Database Test Deploy for Heroku Postgres

---
+++ app/controllers/DatabaseTestController.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import models.DatabaseTest;
+import play.libs.Json;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+import views.html.databaseTest;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class DatabaseTestController extends Controller {
+
+    @Inject
+    FormFactory formFactory;
+
+    public Result returnToDBTest(){
+        Form<DatabaseTest> dbTestForm = formFactory.form(DatabaseTest.class);
+        return ok(databaseTest.render(dbTestForm));
+    }
+
+    // Database function Testing
+    public Result getDBTests() {
+        List<DatabaseTest> dbTests = DatabaseTest.find.all();
+        return ok(Json.toJson(dbTests));
+    }
+
+    public Result saveDatabaseTest(Http.Request request) {
+        Form<DatabaseTest> dbTestForm = formFactory.form(DatabaseTest.class).bindFromRequest(request);
+        DatabaseTest dbTest = dbTestForm.get();
+        dbTest.save();
+        return redirect(routes.HomeController.index());
+    }
+}
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,12 @@
 
 # --- !Ups
 
+create table database_test (
+  id                            bigserial not null,
+  database_message              varchar(255),
+  constraint pk_database_test primary key (id)
+);
+
 create table person (
   id                            bigserial not null,
   name                          varchar(255),
@@ -13,14 +19,29 @@ create table trip_info (
   trip_id                       bigserial not null,
   start_location                varchar(255),
   end_location                  varchar(255),
+  min_stop_count                integer not null,
+  max_stop_count                integer not null,
   stop_count                    integer not null,
   constraint pk_trip_info primary key (trip_id)
+);
+
+create table trip_rating (
+  trip_rating_id                bigserial not null,
+  trip_id                       bigint not null,
+  min_rate_value                integer not null,
+  max_rate_value                integer not null,
+  rating                        integer not null,
+  constraint pk_trip_rating primary key (trip_rating_id)
 );
 
 
 # --- !Downs
 
+drop table if exists database_test cascade;
+
 drop table if exists person cascade;
 
 drop table if exists trip_info cascade;
+
+drop table if exists trip_rating cascade;
 
